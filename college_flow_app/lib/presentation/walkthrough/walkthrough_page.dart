b4a4335a9eea6dd4c8f0badef929083fb84fb8fb
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-07-30 02:16:09 -0300

    refac: componetize walkthrough pages into a common widget

--- college_flow_app/lib/presentation/walkthrough/walkthrough_page.dart
+++ college_flow_app/lib/presentation/walkthrough/walkthrough_page.dart
@@ -1,6 +1,8 @@
-import 'package:college_flow_app/presentation/walkthrough/walkthrough_first_page.dart';
-import 'package:college_flow_app/presentation/walkthrough/walkthrough_second_page.dart';
-import 'package:college_flow_app/presentation/walkthrough/walkthrough_third_page.dart';
+import 'package:college_flow_app/config/constants/assets_contants.dart';
+import 'package:college_flow_app/config/design_system/data/spacing/spacing.dart';
+import 'package:college_flow_app/presentation/walkthrough/widgets/walkthrough_base_page.dart';
+import 'package:college_flow_app/presentation/widgets/buttons/flow_button.dart';
+import 'package:college_flow_app/presentation/widgets/gap.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -14,68 +16,72 @@ class WalkthroughPage extends StatefulWidget {
 class WalkthroughPageState extends State<WalkthroughPage> {
   final _pageViewController = PageController();
 
-  void togglePage(int page) {
-    _pageViewController.animateToPage(
-      page,
-      duration: const Duration(milliseconds: 200),
-      curve: Curves.easeIn,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff2C61F4),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: spacingXXXS,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: _pageViewController,
+                  children: const [
+                    WalkthroughBasePage(
+                      title: "Disciplinas",
+                      description:
+                          "Descubra tudo sobre as disciplinas que está cursando e ainda vai cursar!",
+                      imgPath: walkthrough01,
+                    ),
+                    WalkthroughBasePage(
+                      title: "Avaliações",
+                      description:
+                          "Saiba o que os alunos estão achando de outras disciplinas em tempo real!",
+                      imgPath: walkthrough01,
+                    ),
+                    WalkthroughBasePage(
+                      title: "Ranking",
+                      description:
+                          "Veja quais são as melhores disciplinas de acordo com as notas dadas pelos alunos que cursaram!",
+                      imgPath: walkthrough01,
+                    ),
+                  ],
+                ),
+              ),
+              SmoothPageIndicator(
                 controller: _pageViewController,
-                children: const [
-                  WalkthroughFirstPage(),
-                  WalkthroughSecondPage(),
-                  WalkthroughThirdPage(),
-                ],
+                count: 3,
+                effect: const SwapEffect(
+                  spacing: spacingXXXS,
+                  dotHeight: spacingNano,
+                  dotWidth: spacingNano,
+                  dotColor: Color(0xff234EC3),
+                  activeDotColor: Colors.white,
+                ),
+                onDotClicked: togglePage,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SmoothPageIndicator(
-                    controller: _pageViewController,
-                    count: 3,
-                    effect: const SwapEffect(
-                      spacing: 8,
-                      dotHeight: 10,
-                      dotWidth: 10,
-                      dotColor: Color(0xff234EC3),
-                      activeDotColor: Colors.white,
-                    ),
-                    onDotClicked: togglePage,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      if (_pageViewController.page == 2) {
-                        return;
-                      }
-                      final page = (_pageViewController.page! + 1).toInt();
-                      togglePage(page);
-                    },
-                    color: Colors.white,
-                    icon: const Icon(
-                      Icons.arrow_forward,
-                    ),
-                  ),
-                ],
+              const VSpacer.xxxs(),
+              FlowButton(
+                label: "Vamos Começar",
+                //TODO(Mauricio-Machado): When user clicks redirect to login page
+                onTap: () => togglePage(2),
               ),
-            ),
-            const SizedBox(height: 30),
-          ],
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void togglePage(int page) {
+    _pageViewController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeIn,
     );
   }
 }
---
+++ college_flow_app/lib/presentation/walkthrough/widgets/walkthrough_base_page.dart
@@ -0,0 +1,63 @@
+import 'package:college_flow_app/config/design_system/data/colors/colors.dart';
+import 'package:college_flow_app/config/design_system/data/spacing/spacing.dart';
+import 'package:college_flow_app/presentation/widgets/gap.dart';
+import 'package:flutter/material.dart';
+
+class WalkthroughBasePage extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imgPath;
+
+  const WalkthroughBasePage({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.imgPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: spacingNano,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const VSpacer.lg(),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                  color: white,
+                ),
+          ),
+          const VSpacer.nano(),
+          Text(
+            description,
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  color: white,
+                ),
+          ),
+          const VSpacer.lg(),
+          Stack(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  width: 30,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: primaryLight,
+                  ),
+                ),
+              ),
+              //TODO(Mauricio-Machado): Add image here
+              const SizedBox.shrink(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
