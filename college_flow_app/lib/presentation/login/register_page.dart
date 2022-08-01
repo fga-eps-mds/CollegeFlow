c250a4c5076bc8b4d8d96c4b5b6b04a13ee46c75
Wengel-Rodrigues <wengelmaster@gmail.com>
2022-08-01 16:56:10 -0300

    refac.. update checkbox and register_page

--- college_flow_app/lib/presentation/login/register_page.dart
+++ college_flow_app/lib/presentation/login/register_page.dart
@@ -4,6 +4,7 @@ import 'package:college_flow_app/config/design_system/data/colors/colors.dart';
 import 'package:college_flow_app/config/design_system/data/spacing/spacing.dart';
 import 'package:college_flow_app/presentation/login/register_geral.dart';
 import 'package:college_flow_app/presentation/widgets/buttons/flow_button.dart';
+import 'package:college_flow_app/presentation/widgets/checkboxlist.dart';
 import 'package:college_flow_app/presentation/widgets/gap.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -18,6 +19,10 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   final _pageViewController = PageController();
+
+  bool _student = false;
+  bool _teacher = false;
+  bool _adm = false;
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +65,48 @@ class _RegisterPageState extends State<RegisterPage> {
                     color: textColor,
                   ),
             ),
-            const VSpacer.xxs(),
+            Image.asset('assets/icons/student_icon.png'),
+            const VSpacer.nano(),
+            CheckboxList(
+              label: 'Aluno',
+              padding: const EdgeInsets.symmetric(horizontal: 118.0),
+              value: _student,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _adm = false;
+                  _student = newValue;
+                  _teacher = false;
+                });
+              },
+            ),
+            Image.asset('assets/icons/professor_icon.png'),
+            const VSpacer.nano(),
+            CheckboxList(
+              label: 'Professor',
+              padding: const EdgeInsets.symmetric(horizontal: 108.0),
+              value: _teacher,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _adm = false;
+                  _teacher = newValue;
+                  _student = false;
+                });
+              },
+            ),
+            Image.asset('assets/icons/admin_icon.png'),
+            const VSpacer.nano(),
+            CheckboxList(
+              label: 'Administrador',
+              padding: const EdgeInsets.symmetric(horizontal: 92.0),
+              value: _adm,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _teacher = false;
+                  _adm = newValue;
+                  _student = false;
+                });
+              },
+            ),
             const VSpacer.xxs(),
             FlowButton(
               label: "Continue",
