8dc7a4506a4e9cea7d5b2cabfed706c947d916f4
Wengel-Rodrigues <wengelmaster@gmail.com>
2022-08-01 00:11:46 -0300

    feat.. add_registerPage
    Co-authored-by: Luan Melo <luanmelo.q@gmail.com>

---
+++ college_flow_app/lib/presentation/login/register_page.dart
@@ -0,0 +1,91 @@
+import 'dart:ffi';
+
+import 'package:college_flow_app/config/design_system/data/colors/colors.dart';
+import 'package:college_flow_app/config/design_system/data/spacing/spacing.dart';
+import 'package:college_flow_app/presentation/widgets/buttons/flow_button.dart';
+import 'package:college_flow_app/presentation/widgets/gap.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _pageViewController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: primary,
+      ),
+      backgroundColor: flowColorWhite,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 124,
+              width: double.infinity,
+              decoration: BoxDecoration(color: primary),
+              padding: const EdgeInsets.symmetric(
+                horizontal: spacingXXXS,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/logo.png',
+                  ),
+                  const VSpacer.nano(),
+                  Text(
+                    'Faça seu Cadastro para ter acesso ao Aplicativo!',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: white,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+            const VSpacer.xxs(),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: spacingXXXS,
+              ),
+            ),
+            Text(
+              'Selecione seu tipo de usuário:',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: black,
+                  ),
+            ),
+            const VSpacer.xxs(),
+            FlowButton(
+              label: "Continue",
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    return RegisterPage();
+                  }),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void togglePage(int page) {
+    _pageViewController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeIn,
+    );
+  }
+}
