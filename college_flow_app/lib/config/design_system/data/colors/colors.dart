4acb277a464f7dd9616df3963f3e211a289bde36
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-01 21:16:50 -0300

    feat: add more variants

--- college_flow_app/lib/config/design_system/data/colors/colors.dart
+++ college_flow_app/lib/config/design_system/data/colors/colors.dart
@@ -10,6 +10,7 @@ const flowColorRed = Color.fromRGBO(244, 67, 54, 1);
 const flowColorRed10 = Color.fromARGB(255, 242, 114, 105);
 const textColor = Color.fromARGB(255, 28, 27, 31);
 const white = Colors.white;
+const whiteLight = Color.fromARGB(255, 202, 202, 202);
 const secondary = Color.fromARGB(255, 248, 150, 39);
 const primaryLight = Color.fromRGBO(107, 144, 247, 1);
 const primary = Color.fromARGB(255, 44, 97, 244);
