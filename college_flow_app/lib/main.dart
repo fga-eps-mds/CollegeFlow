0d147d1394427d3d87a0c5f1cf7190f0acdfc8e4
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-11 23:42:39 -0300

    feat: register injection

--- college_flow_app/lib/main.dart
+++ college_flow_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:college_flow_app/config/routes/flow_router.dart';
 import 'package:college_flow_app/config/routes/flow_routes.dart';
 import 'package:college_flow_app/config/themes/custom_theme.dart';
+import 'package:college_flow_app/core/service_locator_manager.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await ServiceLocatorManager.I.init();
   runApp(const MyApp());
 }
 
