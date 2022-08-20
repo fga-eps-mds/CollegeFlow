6b8ad3cd7669ad3d1c9e2b72c6ab5da194c11205
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-20 19:10:29 -0300

    refac: remove auth feature

--- college_flow_app/lib/config/routes/flow_routes.dart
+++ college_flow_app/lib/config/routes/flow_routes.dart
@@ -1,9 +1,3 @@
 abstract class FlowRoutes {
   static const mainPage = '/';
-  static const loginPage = '/LoginPage';
-  static const registerPage = '/registerPage';
-  static const registerGeral = '/registerGeral';
-  static const registerStudent = '/registerStudent';
-  static const registerProfessor = '/registerProfessor';
-  static const registerAdmin = '/registerAdmin';
 }
