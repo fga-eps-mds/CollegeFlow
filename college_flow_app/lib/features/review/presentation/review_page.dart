eb18e622ee332689b4f608f99ef53e3ae77321cd
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-21 20:55:25 -0300

    refac: fix overflow

--- college_flow_app/lib/features/review/presentation/review_page.dart
+++ college_flow_app/lib/features/review/presentation/review_page.dart
@@ -1,17 +1,12 @@
 import 'package:college_flow_app/config/design_system/data/colors/colors.dart';
-import 'package:college_flow_app/config/design_system/data/icons/sizes.dart';
-import 'package:college_flow_app/config/design_system/data/object_styles/border_radius.dart';
 import 'package:college_flow_app/config/design_system/data/spacing/spacing.dart';
 import 'package:college_flow_app/features/review/list_example.dart';
 import 'package:college_flow_app/features/review/presentation/widgets/review_card.dart';
+import 'package:college_flow_app/features/review/presentation/widgets/subject_card.dart';
 import 'package:college_flow_app/shared/widgets/buttons/flow_button.dart';
 import 'package:college_flow_app/shared/widgets/flow_icon.dart';
-import 'package:college_flow_app/shared/widgets/flow_icon_button.dart';
 import 'package:college_flow_app/shared/widgets/gap.dart';
 import 'package:flutter/material.dart';
-
-part './widgets/subject_card.dart';
-part './widgets/review_score_tag.dart';
 
 class ReviewPage extends StatefulWidget {
   const ReviewPage({Key? key}) : super(key: key);
@@ -23,28 +18,74 @@ class ReviewPage extends StatefulWidget {
 class _ReviewPageState extends State<ReviewPage> {
   final String _mockSubjectName = 'any_subject';
   final String _mockSubjectCode = 'any_code';
-  final double _scoreAmount = 3.2;
-
-  final Review _mockReview = Review(
-    voteCounter: 25,
-    teacherName: 'NOME PROFESSOR',
-    score: 3.0,
-    title: 'Lorem ipsum dolor sit amet, consectetu!',
-    description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod mi sollicitudin fringilla auctor. Phasellus sit amet gravida sem, vitae fermentum nulla. Suspendisse a leo vestibulum, tempus lacus quis, molestie urna.',
-  );
+  final double _scoreMean = 3.2;
 
   final List<Review> _mockReviewList = reviewList;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: colorPrimary,
-        elevation: 0,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: colorPrimary,
+      //   elevation: 0,
+      // ),
       body: Stack(
         children: [
+          CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                backgroundColor: colorPrimary,
+                elevation: 0,
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    SubjectCard(
+                      reviewScore: _scoreMean,
+                      subjectCode: _mockSubjectCode,
+                      subjectName: _mockSubjectName,
+                    ),
+                    const VSpacer.xs(),
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: spacingXXS),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Avaliações',
+                              style: Theme.of(context).textTheme.headlineMedium,
+                            ),
+                          ),
+                          const Divider(
+                            thickness: spacingQuarck,
+                            color: colorSecondary,
+                          ),
+                          ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: _mockReviewList.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  ReviewCard(
+                                    review: _mockReviewList[index],
+                                  ),
+                                  const VSpacer.xxxs(),
+                                ],
+                              );
+                            },
+                          ),
+                          const VSpacer.sm(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(
@@ -55,40 +96,11 @@ class _ReviewPageState extends State<ReviewPage> {
                 child: FlowButton(
                   label: 'Criar avaliação',
                   suffixIcon: const FlowIcon.editComment(),
+                  //TODO(Mauricio-Machado): Navigate to Review Form
                   onTap: () {},
                 ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              SubjectCard(
-                reviewScore: _scoreAmount,
-                subjectCode: _mockSubjectCode,
-                subjectName: _mockSubjectName,
-              ),
-              const VSpacer.xs(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: spacingXXS),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Avaliações',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                    ),
-                    const Divider(
-                      thickness: spacingQuarck,
-                      color: colorSecondary,
-                    ),
-                    ReviewCard(review: _mockReview),
-                    const VSpacer.xxs(),
-                  ],
-                ),
-              ),
-            ],
           ),
         ],
       ),
