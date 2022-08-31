b4d9372687b899129c4d4d0a02f081a2a34ae9ac
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-30 21:29:30 -0300

    feat: inject subject service

--- college_flow_app/lib/features/list_subjects/subject_injection_container.dart
+++ college_flow_app/lib/features/list_subjects/subject_injection_container.dart
@@ -1,11 +1,19 @@
 import 'package:college_flow_app/core/injection_container.dart';
 import 'package:college_flow_app/features/list_subjects/domain/usecase/get_subjects_list.dart';
+import 'package:college_flow_app/features/list_subjects/infra/subjects_api_service.dart';
 import 'package:get_it/get_it.dart';
 
 class SubjectInjectionContainer implements InjectionContainer {
   @override
   Future<void> inject(GetIt serviceLocator) async {
+    await injectServices(serviceLocator);
     await injectUsecases(serviceLocator);
+  }
+
+  Future<void> injectServices(GetIt serviceLocator) async {
+    serviceLocator.registerFactory<SubjectAPIService>(
+      () => SubjectAPIService(serviceLocator()),
+    );
   }
 
   Future<void> injectUsecases(GetIt serviceLocator) async {
