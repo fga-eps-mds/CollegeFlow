503fd8e40efa8876012e555145988223f1982fd4
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-31 09:37:29 -0300

    feat: add injections in container

--- college_flow_app/lib/features/subject/subject_injection_container.dart
+++ college_flow_app/lib/features/subject/subject_injection_container.dart
@@ -1,5 +1,9 @@
 import 'package:college_flow_app/core/injection_container.dart';
+import 'package:college_flow_app/features/subject/data/datasources/subject_datasource.dart';
+import 'package:college_flow_app/features/subject/data/repositories/subject_repository_impl.dart';
+import 'package:college_flow_app/features/subject/domain/repositories/subject_repository.dart';
 import 'package:college_flow_app/features/subject/domain/usecase/get_subjects_list.dart';
+import 'package:college_flow_app/features/subject/infra/datasources/subject_datasource_impl.dart';
 import 'package:college_flow_app/features/subject/infra/subjects_api_service.dart';
 import 'package:get_it/get_it.dart';
 
@@ -7,6 +11,8 @@ class SubjectInjectionContainer implements InjectionContainer {
   @override
   Future<void> inject(GetIt serviceLocator) async {
     await injectServices(serviceLocator);
+    await injectDatasources(serviceLocator);
+    await injectRepositories(serviceLocator);
     await injectUsecases(serviceLocator);
   }
 
@@ -16,7 +22,20 @@ class SubjectInjectionContainer implements InjectionContainer {
     );
   }
 
+  Future<void> injectDatasources(GetIt serviceLocator) async {
+    serviceLocator.registerFactory<SubjectDatasource>(
+      () => SubjectDatasourceImpl(apiService: serviceLocator()),
+    );
+  }
+
+  Future<void> injectRepositories(GetIt serviceLocator) async {
+    serviceLocator.registerFactory<SubjectRepository>(
+      () => SubjectRepositoryImpl(subjectDatasource: serviceLocator()),
+    );
+  }
+
   Future<void> injectUsecases(GetIt serviceLocator) async {
-    serviceLocator.registerFactory<GetSubjectList>(() => GetSubjectListImpl());
+    serviceLocator.registerFactory<GetSubjectList>(
+        () => GetSubjectListImpl(subjectRepository: serviceLocator()));
   }
 }
