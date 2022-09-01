cf00b35ce81d147a0547fb9af7f8cf3d2e170764
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-09-01 10:15:25 -0300

    refac: implement repository on usecase

--- college_flow_app/lib/features/review/domain/usecases/get_review_lists.dart
+++ college_flow_app/lib/features/review/domain/usecases/get_review_lists.dart
@@ -1,4 +1,5 @@
 import 'package:college_flow_app/core/failures.dart';
+import 'package:college_flow_app/features/review/domain/repositories/review_repository.dart';
 import 'package:dartz/dartz.dart';
 
 import '../entities/review.dart';
@@ -8,22 +9,12 @@ abstract class GetReviewList {
 }
 
 class GetReviewListImpl implements GetReviewList {
+  final ReviewRepository reviewRepository;
+
+  GetReviewListImpl({required this.reviewRepository});
+
   @override
   Future<Either<Failure, List<Review>>> call() async {
-    //TODO(Mauricio-Machado): Implement usecase call
-    return Right(reviewList);
+    return await reviewRepository.getReviews();
   }
 }
-
-//TODO(Mauricio-Machado): Remove when implemented usecase
-final List<Review> reviewList = List.generate(
-  12,
-  (index) => Review(
-    voteCounter: 25,
-    teacherName: 'NOME PROFESSOR $index',
-    score: 3.0,
-    title: 'Lorem ipsum dolor sit amet, consectetu!',
-    description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod mi sollicitudin fringilla auctor. Phasellus sit amet gravida sem, vitae fermentum nulla. Suspendisse a leo vestibulum, tempus lacus quis, molestie urna.',
-  ),
-);
