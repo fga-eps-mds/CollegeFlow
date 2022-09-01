2d0a7e274fb7b9604cfc513d8f35efed38ad7436
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-31 21:27:27 -0300

    feat: test datasource subject

--- college_flow_app/test/features/subject/data/repositories/subject_repository_impl_test.dart
+++ college_flow_app/test/features/subject/data/repositories/subject_repository_impl_test.dart
@@ -1,15 +1,17 @@
+import 'package:collection/collection.dart';
 import 'package:college_flow_app/core/failures.dart';
 import 'package:college_flow_app/features/subject/data/repositories/subject_repository_impl.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../mocks/subject_datasource_mock.dart';
+import '../../mocks/subject_entity_factory.dart';
 
 void main() {
   late final SubjectRepositoryImpl sut;
   late final SubjectDatasourceMock datasourceMock;
 
-  setUp(() {
+  setUpAll(() {
     datasourceMock = SubjectDatasourceMock();
     sut = SubjectRepositoryImpl(
       subjectDatasource: datasourceMock,
@@ -18,17 +20,41 @@ void main() {
 
   group('FAILED', () {
     test(
-        'When get subject list throws exception, repository should return corresponding failure',
-        () async {
-      //arrange?
-      const expectedResult = Left(ServerFailure());
-      datasourceMock.mockGetSubjectsFail();
+      'When get subject list throws exception, repository should return corresponding failure',
+      () async {
+        //arrange?
+        const expectedResult = Left(ServerFailure());
+        datasourceMock.mockGetSubjectsFail();
 
-      //* act
-      final result = await sut.getReviewList();
+        //* act
+        final result = await sut.getSubjects();
 
-      //assert
-      expect(result, expectedResult);
-    });
+        //assert
+        expect(result, expectedResult);
+      },
+    );
+  });
+
+  group('SUCESS', () {
+    test(
+      'should return List of subjects models when requests succedes',
+      () async {
+        //arrange?
+        Function listEquality = const ListEquality().equals;
+        final expectedResult = SubjectEntityFactory.buildList();
+        datasourceMock.mockGetSubjectsSuccess();
+
+        //* act
+        final result = await sut.getSubjects();
+
+        //assert
+        expect(result.isRight(), true);
+        result.map(
+          (subjects) {
+            expect(listEquality(subjects, expectedResult), true);
+          },
+        );
+      },
+    );
   });
 }
