d4daf46acd2dd39651667aefb79f1f21d99de146
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-08-31 21:26:03 -0300

    refac: change expection param to dynamic

--- college_flow_app/lib/core/error_logger.dart
+++ college_flow_app/lib/core/error_logger.dart
@@ -2,8 +2,7 @@ import 'package:college_flow_app/core/exceptions.dart';
 import 'package:flutter/material.dart';
 
 class FlowLogger {
-  static void showError(
-      String where, String function, FlowException exception) {
+  static void showError(String where, String function, exception) {
     debugPrint('-----EXCEPTION LOG-----');
     debugPrint('🔴There was an error.🔴');
     debugPrint('-----------------------');
