4734183700c8761076823ee2f1c58933f302f1e6
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-09-02 20:53:32 -0300

    feat: test review datasource

---
+++ college_flow_app/test/features/review/mocks/review_api_service_mock.dart
@@ -0,0 +1,31 @@
+import 'package:college_flow_app/features/review/infra/review_api_service.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:dio/dio.dart';
+
+import 'get_review_response_model_factory.dart';
+
+class ReviewAPIServiceMock extends Mock implements ReviewAPIService {
+  final dioError = DioError(
+    requestOptions: RequestOptions(path: '/subject'),
+    response: Response(
+      requestOptions: RequestOptions(path: '/subject'),
+      statusCode: 500,
+    ),
+    error: 'any_error_message',
+  );
+
+  final error = Error();
+
+  When _mockGetReviews() => when(
+        () => getReviews(code: any(named: 'code')),
+      );
+
+  //Get Reviews
+  void mockDioFail() => _mockGetReviews().thenThrow(dioError);
+
+  void mockGeneralFail() => _mockGetReviews().thenThrow(error);
+
+  void mockSucess() => _mockGetReviews().thenAnswer(
+        (_) async => GetReviewResponseFactory.build(),
+      );
+}
