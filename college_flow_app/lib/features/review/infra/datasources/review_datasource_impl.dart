09b4364913cb81e0af871fe82afb9bce59696715
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-09-02 20:53:01 -0300

    refac: remove dead code

--- college_flow_app/lib/features/review/infra/datasources/review_datasource_impl.dart
+++ college_flow_app/lib/features/review/infra/datasources/review_datasource_impl.dart
@@ -6,18 +6,6 @@ import 'package:college_flow_app/features/review/infra/review_api_service.dart';
 import 'package:dio/dio.dart';
 
 class ReviewDatasourceImpl implements ReviewDatasource {
-  //TODO(Mauricio-Machado): Remove when implemented api service request
-  final List<ReviewModel> reviewList = List.generate(
-    12,
-    (index) => ReviewModel(
-      professor: 'NOME PROFESSOR $index',
-      rating: 3.0,
-      title: 'Lorem ipsum dolor sit amet, consectetu!',
-      comment:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod mi sollicitudin fringilla auctor. Phasellus sit amet gravida sem, vitae fermentum nulla. Suspendisse a leo vestibulum, tempus lacus quis, molestie urna.',
-    ),
-  );
-
   final ReviewAPIService apiService;
 
   ReviewDatasourceImpl({required this.apiService});
