b692cbbfaf64a6414691e0db76031d088405d844
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-09-05 09:45:17 -0300

    feat: add reset event

--- college_flow_app/lib/features/review/presentation/bloc/load_review_list_bloc.dart
+++ college_flow_app/lib/features/review/presentation/bloc/load_review_list_bloc.dart
@@ -30,6 +30,9 @@ class LoadReviewListBloc
               ),
             );
           },
+          reset: () {
+            emit(const LoadReviewListState.loading());
+          },
         );
       },
     );
